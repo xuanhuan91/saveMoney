1b651867716cf4492082f9f6349dbd37140ddcfb
xuanhuan91 <xuanhuan91@gmail.com>
2022-08-30 23:36:25 +0700

    huan-dev

---
+++ app/Http/Controllers/CategoryIncomeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\categoryIncome;
+use Illuminate\Http\Request;
+
+class CategoryIncomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\categoryIncome  $categoryIncome
+     * @return \Illuminate\Http\Response
+     */
+    public function show(categoryIncome $categoryIncome)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\categoryIncome  $categoryIncome
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(categoryIncome $categoryIncome)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\categoryIncome  $categoryIncome
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, categoryIncome $categoryIncome)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\categoryIncome  $categoryIncome
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(categoryIncome $categoryIncome)
+    {
+        //
+    }
+}
