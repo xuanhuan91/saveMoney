b9e216e793a6f3394e0ed83ed2ba69539923ba58
tiennvth2108051 <96479529+tiennvth2108051@users.noreply.github.com>
2022-09-05 09:36:50 +0700

    Coding categoryExpense model

--- app/Models/categoryExpense.php
+++ app/Models/categoryExpense.php
@@ -12,4 +12,12 @@ class categoryExpense extends Model
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
+    protected $fillable = ['name', 'parent_id'];
+
+    public function sub_category(){
+        return $this->hasMany(categoryExpense::class, 'parent_id');
+    }
+    public function self_category(){
+        return $this->belongsTo(categoryExpense::class, 'parent_id');
+    }
 }
