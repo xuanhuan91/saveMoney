fb9165ee3983ff3e8baa9636ff5019231bd95baa
xuanhuan91 <xuanhuan91@gmail.com>
2022-09-07 09:05:43 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,3 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/categoryExpense', 'App\Http\Controllers\CategoryExpenseController@index');
