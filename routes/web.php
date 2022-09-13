5bbf1dc914017487cfee040bccd25ef4a02d445a
tiennvth2108051 <96479529+tiennvth2108051@users.noreply.github.com>
2022-09-13 20:14:41 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/report/month', [App\Http\Controllers\ReportController::class, 'reportByMonth'])->name('report-month');
 Route::get('/report/week', [App\Http\Controllers\ReportController::class, 'reportByWeek'])->name('report-week');
-Route::get('/categoryExpense', 'CategoryExpenseController@index');
+//Route::get('/categoryExpense', 'CategoryExpenseController@index');
 
 Route::get('/login', function (){return view('auth.login')->name('login');});
 Route::get('/register',function (){return view('auth.register');})->name('register');
@@ -25,7 +25,8 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('user', \App\Http\Controllers\userController::class);
-Route::get('/categoryExpense', 'App\Http\Controllers\CategoryExpenseController@index');
+//Route::resource("CategoryExpense", App\Http\Controllers\categoryExpenseController::class);
+//Route::get('/categoryExpense', 'App\Http\Controllers\CategoryExpenseController@index');
 Route::resource("CategoryExpense", App\Http\Controllers\categoryExpenseController::class);
 Route::resource("expense", App\Http\Controllers\ExpenseController::class);
 
