0f4c879bb988c92bd43f4c08bc88794f6802cf30
nguyenbinhND <binhnv7496@gmail.com>
2022-09-26 08:57:07 +0700

    merge binhdev into main

--- resources/views/income/create.blade.php
+++ resources/views/income/create.blade.php
@@ -46,34 +46,34 @@
                             </script>
 
 
-                            test 1
+{{--                            test 1--}}
 
 
-                <div class="form-group ">
-                    <label for="income_category">Loại khoản thu</label>
-                    <select class="js-example-basic-single form-control" id="income_category" name="income_category">
-                        @foreach($lscategoryincome as $lscategory)
-                            <option value="{{$lscategory->id}}">{{$lscategory->name}}</option>
-                        @endforeach
+{{--                <div class="form-group ">--}}
+{{--                    <label for="income_category">Loại khoản thu</label>--}}
+{{--                    <select class="js-example-basic-single form-control" id="income_category" name="income_category">--}}
+{{--                        @foreach($lscategoryincome as $lscategory)--}}
+{{--                            <option value="{{$lscategory->id}}">{{$lscategory->name}}</option>--}}
+{{--                        @endforeach--}}
 
-                    </select>
-                </div>
+{{--                    </select>--}}
+{{--                </div>--}}
 
-                <div class="form-group ">
-                    <label for="income_category">Lựa chọn thành phần loại khoản thu</label>
-                    <select class="js-example-basic-single form-control" name="income_category_id" id="subincome_category">>
-                        @foreach($subcategory as $subidcategory)
-                            <option value="{{$subidcategory->id}}">{{$subidcategory->name}}</option>
-                        @endforeach
-                    </select>
-                </div>
+{{--                <div class="form-group ">--}}
+{{--                    <label for="income_category">Lựa chọn thành phần loại khoản thu</label>--}}
+{{--                    <select class="js-example-basic-single form-control" name="income_category_id" id="subincome_category">>--}}
+{{--                        @foreach($subcategory as $subidcategory)--}}
+{{--                            <option value="{{$subidcategory->id}}">{{$subidcategory->name}}</option>--}}
+{{--                        @endforeach--}}
+{{--                    </select>--}}
+{{--                </div>--}}
 
-                <script>
-                    $(document).ready(function() {
-                        $('.js-example-basic-single').select2();
-                    });
-                </script>
-                            endtest
+{{--                <script>--}}
+{{--                    $(document).ready(function() {--}}
+{{--                        $('.js-example-basic-single').select2();--}}
+{{--                    });--}}
+{{--                </script>--}}
+{{--                            endtest--}}
 
 
                 <div class="form-group">
