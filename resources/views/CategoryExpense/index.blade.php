ea1f9807d130fa7768b992b7f283c4c3096ab980
tiennvth2108051 <96479529+tiennvth2108051@users.noreply.github.com>
2022-09-27 05:54:52 +0700

    test

--- resources/views/CategoryExpense/index.blade.php
+++ resources/views/CategoryExpense/index.blade.php
@@ -1,59 +1,105 @@
 @extends('layouts.app')
     @section('content')
-{{--        <ul>--}}
-{{--            <li>--}}
-{{--                <a href="{{route('CategoryExpense.create')}}"><button class="btn btn-warning">New category</button></a>--}}
-{{--            </li>--}}
-{{--        </ul>--}}
-{{--        @foreach($parentCategoryExpense as $parentCategory)--}}
-{{--            <ul class="list-group">--}}
-{{--                <li class="list-group-item"><a>{{$parentCategory->name}}</a>--}}
-{{--                <li class="list-group-item">--}}
-{{--                    @if(count($parentCategory->subcategory))--}}
-{{--                        @include('CategoryExpense.sub_category_list',['subcategories' => $parentCategory->subcategory])--}}
-{{--                    @endif--}}
-{{--                </li>--}}
-{{--                </li>--}}
-{{--            </ul>--}}
-{{--        @endforeach--}}
-    <div style="margin: 30px">
-        <h1 class="display-7">Quản lý loại khoản chi</h1>
-        <a href="{{route('CategoryExpense.create')}}">
-            <h4 class="text-right"><button class="btn btn-warning">Thêm</button></h4>
-        </a>
+        <div style="margin: 30px">
+            <h1 class="display-7">Quản lý loại khoản chi</h1>
+            <div>
+                <a href="{{route('CategoryExpense.create')}}">
+                    <h4 class="text-xl-right">
+                        <button class="btn btn-primary">Thêm</button>
+                    </h4>
+                </a>
+            </div>
 
-        <table class="table">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">Tên loại khoản chi</th>
-                <th scope="col">Khoản chi con</th>
-                <th scope="col">Thời gian tạo</th>
-                <th scope="col">Sửa</th>
-                <th scope="col">Xóa</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($parentCategoryExpense as $parentCategory)
-            <tr>
-                <td>{{$parentCategory->name}}</td>
-                @if(count($parentCategory->subcategory))
-                    <td>@include('CategoryExpense.sub_category_list',['subcategories' => $parentCategory->subcategory])</td>
-                @endif
-                <td>{{$parentCategory->created_at}}</td>
-                <td><a class="btn btn-warning">Edit</a></td>
-                <td>
-                    <form method="post" action="{{route('CategoryExpense.destroy', $parentCategory->id)}}"
-                                                  onsubmit='return confirm("Are you sure ?")'>
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="Delete" class="btn btn-danger">
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-            </tbody>
-        </table>
+            <table class="table">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Tên loại khoản chi</th>
+                    <th scope="col">Thành Phần Loại Khoản Chi</th>
+                    <th scope="col">Thời gian tạo</th>
+                    <th class="text-right">Sửa</th>
+                    <th class="text-md-center">Xóa</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($lsCategoryExpense as $parentExpense)
+                    <tr>
+                        <td>{{$parentExpense->name}}</td>
+                        <td>{{$parentExpense->subCategoryiD}}</td>
+                        <td>{{$parentExpense->created_at}}</td>
+                        <td class="text-right">
+                            <a  class="btn btn-warning" href="{{route("CategoryExpense.edit", $parentExpense->id)}}">Edit</a>
+                        <td class="text-xl-center"><form method="post" action="{{route('CategoryExpense.destroy', $parentExpense->id)}}"
+                                                         onsubmit='return confirm("Are you sure?")'>
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete" class="btn btn-danger">
+                            </form></td>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <script type="text/javascript">
+            function confirmDelete() {
+                var value =  confirm("Sure ?");
+                return value;
+            }
+        </script>
+    @endsection
+@section('modalBody')
+    <div class="modal-body">
+        <h5 class="modal-title fw-bold text-center" id="exampleModalLabel">Edit Category Expense</h5>
+
+        <form method="post"  action="{{route('CategoryExpense.update',$parentExpense->id)}}">
+            @csrf
+            @method('put')
+            <div>
+                <div>
+                    <label for="name" class="col-md-12 mb-0 mt-2">{{ __('Name') }}</label>
+                    <div class="col-md-12">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                               value="{{ old('name',$parentExpense->name) }}" required autocomplete="name" autofocus>
+
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div>
+                    <label for="subCategoryiD" class="col-md-12 mb-0 mt-2">{{ __('subCategoryiD') }}</label>
+
+                    <div class="col-md-12">
+                        <input id="subCategoryiD" type="text" class="form-control @error('subCategoryiD') is-invalid @enderror"
+                               name="subCategoryiD" value="{{ old('subCategoryiD',$parentExpense->subCategoryiD) }}" required autocomplete="subCategoryiD">
+
+                        @error('subCategoryiD')
+                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                            </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="row justify-content-around">
+                        <div class="col-4">
+                            <button type="button" class="col btn btn-outline-primary mb-0 mt-2" data-dismiss="modal">
+                                Cancel
+                            </button>
+                        </div>
+                        <div class="col-4">
+                            <button type="submit" class="col btn btn-primary mb-0 mt-2">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 @endsection
+
+
 
 
