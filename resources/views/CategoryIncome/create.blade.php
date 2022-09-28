15cf6564377cdcc89ac8aa2f65ffa910275a1515
TongDuc <85749425+tongducarsenal@users.noreply.github.com>
2022-09-29 00:50:28 +0700

    Layout

--- resources/views/CategoryIncome/create.blade.php
+++ resources/views/CategoryIncome/create.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.app')
-
-@section('content')
+@section('modalBody')
     <div class="container">
-        <div class="container">
-            @if(count($errors) > 0)
-                <div class="alert alert-danger">
-                    @foreach($errors->all() as $err)
-                        <p>{{$err}}</p>
-                    @endforeach
-                </div>
-            @endif
+        @if(count($errors) >0)
+            <div class="alert alert-danger">
+                @foreach($errors->all() as $err)
+                    <p>{{$err}}</p>
+                @endforeach
+            </div>
+        @endif
         <form method="post" action="{{route('CategoryIncome.store')}}">
             @csrf
-            <div class="form-group">
+            <div class="form-group">--}}
                 <label>Tên Loại Khoản Thu</label>
                 <input  class="form-control" type="text" name="name" value="{{old('name')}}"/>
             </div>
@@ -20,10 +18,62 @@
                 <label>Thành Phần loại Khoản Thu</label>
                 <input  class="form-control" type="text" name="subCategoryiD" value="{{old('subCategoryiD')}}"/>
             </div>
-            <div class="form-group py-1">
-                <input type="submit" class="btn btn-primary" value="Save"/>
-                <input href="CategoryIncome.index" type="submit" class="btn btn-danger" value="Cancel"/>
+            <script>
+                $(document).ready(function() {
+                    $('.js-example-basic-single').select2();
+                });
+            </script>
+            test 1
+            <div class="form-group">--}}
+                <label>Tên Loại Khoản Thu</label>
+                <input  class="form-control" type="text" name="name" value="{{old('name')}}"/>
             </div>
+            <div class="form-group">
+                <label>Thành Phần loại Khoản Thu</label>
+                <input  class="form-control" type="text" name="subCategoryiD" value="{{old('subCategoryiD')}}"/>
+            </div>
+            <script>
+                $(document).ready(function() {
+                    $('.js-example-basic-single').select2();
+                });
+            </script>
+            endtest
+            <div>
+                <input style="margin: 10px auto" type="submit" class="btn-warning" value="Cancel">
+                <input href="CategoryIncome.index" style="margin: 10px auto" type="submit" class="btn btn-primary " value="Save">
+            </div>
+
         </form>
     </div>
 @endsection
+@section('content')
+@endsection
+{{--@extends('layouts.app')--}}
+
+{{--@section('content')--}}
+{{--    <div class="container">--}}
+{{--        <div class="container">--}}
+{{--            @if(count($errors) > 0)--}}
+{{--                <div class="alert alert-danger">--}}
+{{--                    @foreach($errors->all() as $err)--}}
+{{--                        <p>{{$err}}</p>--}}
+{{--                    @endforeach--}}
+{{--                </div>--}}
+{{--            @endif--}}
+{{--        <form method="post" action="{{route('CategoryIncome.store')}}">--}}
+{{--            @csrf--}}
+{{--            <div class="form-group">--}}
+{{--                <label>Tên Loại Khoản Thu</label>--}}
+{{--                <input  class="form-control" type="text" name="name" value="{{old('name')}}"/>--}}
+{{--            </div>--}}
+{{--            <div class="form-group">--}}
+{{--                <label>Thành Phần loại Khoản Thu</label>--}}
+{{--                <input  class="form-control" type="text" name="subCategoryiD" value="{{old('subCategoryiD')}}"/>--}}
+{{--            </div>--}}
+{{--            <div class="form-group py-1">--}}
+{{--                <input type="submit" class="btn btn-primary" value="Save"/>--}}
+{{--                <input href="CategoryIncome.index" type="submit" class="btn btn-danger" value="Cancel"/>--}}
+{{--            </div>--}}
+{{--        </form>--}}
+{{--    </div>--}}
+{{--@endsection--}}
--- resources/views/CategoryIncome/index.blade.php
+++ resources/views/CategoryIncome/index.blade.php
@@ -1,16 +1,26 @@
 @extends('layouts.app')
+
 @section('content')
-    <div style="margin: 30px">
-        <h1 class="display-7">Quản lý loại khoản thu</h1>
-        <div>
-            <a href="{{route('CategoryIncome.create')}}">
-                <h4 class="text-xl-right">
-                    <button class="btn btn-primary">Thêm</button>
-                </h4>
-            </a>
+    <div class="container" style="margin-top: 40px">
+        <h4>Quản lý Khoản Thu </h4>
+        <a>
+            <button class="btn btn-primary btn-block " style="width: 10%; float: right"
+                    data-toggle="modal"
+                    data-target="#exampleModal">Thêm
+            </button>
+        </a>
+        <div class="flash-message">
+
+            @foreach(['danger', 'success', 'warning', 'info'] as $type)
+                @if(\Illuminate\Support\Facades\Session::has($type))
+                    <p class="alert alert-{{$type}}">
+                        {{\Illuminate\Support\Facades\Session::get($type)}}
+                    </p>
+                @endif
+            @endforeach
         </div>
 
-        <table class="table">
+        <table class="table">--}}
             <thead class="thead-dark">
             <tr>
                 <th scope="col">Tên loại khoản thu</th>
@@ -26,76 +36,191 @@
                     <td>{{$parentIncome->name}}</td>
                     <td>{{$parentIncome->subCategoryiD}}</td>
                     <td>{{$parentIncome->created_at}}</td>
-                    <td class="text-right">
-                        <a  class="btn btn-warning" href="{{route("CategoryIncome.edit", $parentIncome->id)}}">Edit</a>
-                        <td class="text-xl-center"><form method="post" action="{{route('CategoryIncome.destroy', $parentIncome->id)}}"
-                                  onsubmit='return confirm("Are you sure ?")'>
+                    <td>
+                        <div class="input-group">
+
+                            <a
+                                class="btn btn-info"   href='{{route("CategoryIncome.edit", $parentIncome->id)}}'
+                                style=" --bs-btn-color: #fff;
+                                        --bs-btn-bg: #0d6efd;
+                                        --bs-btn-border-color: #0a58ca;
+                                        --bs-btn-hover-color: #fff;
+                                        --bs-btn-hover-bg: #0a58ca;
+                                        --bs-btn-hover-border-color: #0a58ca;
+                                        --bs-btn-focus-shadow-rgb: 11, 172, 204;
+                                        --bs-btn-active-color: #fff;
+                                        --bs-btn-active-bg: #0a58ca;
+                                        --bs-btn-active-border-color: #0a58ca;
+                                        --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+                                        --bs-btn-disabled-color: #fff;
+                                        --bs-btn-disabled-bg: #0a58ca;
+                                        --bs-btn-disabled-border-color: #0a58ca;"
+                            >Edit</a>
+                            <span style="margin-left: 2px"></span>
+                            <form method="post" action="{{route('CategoryIncome.destroy', $parentIncome->id)}}"
+                                  onsubmit='return confirm("You want to delete ??")'>
                                 @csrf
                                 @method('DELETE')
-                                <input type="submit" value="Delete" class="btn btn-danger">
-                            </form></td>
+                                <input class="btn btn-danger" type="submit" value="Delete" >
+                            </form>
+                        </div>
                     </td>
                 </tr>
             @endforeach
-            </tbody>
         </table>
+
     </div>
+
     <script type="text/javascript">
         function confirmDelete() {
-            var value =  confirm("Sure ?");
+            var value = confirm("You want to delete ?");
             return value;
         }
     </script>
 @endsection
+
 @section('modalBody')
-    <div class="modal-body">
-        <h5 class="modal-title fw-bold text-center" id="exampleModalLabel">Edit CategoryIncome</h5>
-
-        <form method="post"  action="{{route('CategoryIncome.update',$parentIncome->id)}}">
+    <div class="container">
+        @if(count($errors) >0)
+            <div class="alert alert-danger">
+                @foreach($errors->all() as $err)
+                    <p>{{$err}}</p>
+                @endforeach
+            </div>
+        @endif
+        <form method="post" action="{{route('CategoryIncome.store')}}">
             @csrf
-            @method('put')
+            <div class="form-group">--}}
+                <label for="name">Tên Loại Khoản Thu</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{old('name') }}" placeholder="Enter name"/>
+            </div>
+            <div class="form-group">
+                <label for="subCategoryiD">Thành Phần loại Khoản Thu </label>
+                <input type="text" class="form-control" id="subCategoryiD" name="subCategoryiD"{{old('subCategoryiD') }}/>
+            </div>
             <div>
-                <div>
-                    <label for="name" class="col-md-12 mb-0 mt-2">{{ __('Name') }}</label>
-                    <div class="col-md-12">
-                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
-                               value="{{ old('name',$parentIncome->name) }}" required autocomplete="name" autofocus>
-
-                        @error('name')
-                        <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div>
-                    <label for="subCategoryiD" class="col-md-12 mb-0 mt-2">{{ __('subCategoryiD') }}</label>
-
-                    <div class="col-md-12">
-                        <input id="subCategoryiD" type="text" class="form-control @error('subCategoryiD') is-invalid @enderror"
-                               name="subCategoryiD" value="{{ old('subCategoryiD',$parentIncome->subCategoryiD) }}" required autocomplete="subCategoryiD">
-
-                        @error('subCategoryiD')
-                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                            </span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="row justify-content-around">
-                        <div class="col-4">
-                            <button type="button" class="col btn btn-outline-primary mb-0 mt-2" data-dismiss="modal">
-                                Cacel
-                            </button>
-                        </div>
-                        <div class="col-4">
-                            <button type="submit" class="col btn btn-primary mb-0 mt-2">Save</button>
-                        </div>
-                    </div>
-                </div>
+                <input style="margin-right: 15px" type="submit" class="btn btn-primary " value="Save">
+                <a
+                    class="btn btn-info"   href='{{route("CategoryIncome.index")}}'
+                    style=" --bs-btn-color: #fff;
+                                        --bs-btn-bg: #0d6efd;
+                                        --bs-btn-border-color: #0a58ca;
+                                        --bs-btn-hover-color: #fff;
+                                        --bs-btn-hover-bg: #0a58ca;
+                                        --bs-btn-hover-border-color: #0a58ca;
+                                        --bs-btn-focus-shadow-rgb: 11, 172, 204;
+                                        --bs-btn-active-color: #fff;
+                                        --bs-btn-active-bg: #0a58ca;
+                                        --bs-btn-active-border-color: #0a58ca;
+                                        --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+                                        --bs-btn-disabled-color: #fff;
+                                        --bs-btn-disabled-bg: #0a58ca;
+                                        --bs-btn-disabled-border-color: #0a58ca;"
+                >Hủy</a>
             </div>
         </form>
     </div>
 @endsection
+
+
+{{--@extends('layouts.app')--}}
+{{--@section('content')--}}
+{{--    <div style="margin: 30px">--}}
+{{--        <h1 class="display-7">Quản lý loại khoản thu</h1>--}}
+{{--        <div>--}}
+{{--            <a href="{{route('CategoryIncome.create')}}">--}}
+{{--                <h4 class="text-xl-right">--}}
+{{--                    <button class="btn btn-primary">Thêm</button>--}}
+{{--                </h4>--}}
+{{--            </a>--}}
+{{--        </div>--}}
+
+{{--        <table class="table">--}}
+{{--            <thead class="thead-dark">--}}
+{{--            <tr>--}}
+{{--                <th scope="col">Tên loại khoản thu</th>--}}
+{{--                <th scope="col">Thành Phần Loại Khoản Thu</th>--}}
+{{--                <th scope="col">Thời gian tạo</th>--}}
+{{--                <th class="text-right">Sửa</th>--}}
+{{--                <th class="text-md-center">Xóa</th>--}}
+{{--            </tr>--}}
+{{--            </thead>--}}
+{{--            <tbody>--}}
+{{--            @foreach($lsCategoryIncome as $parentIncome)--}}
+{{--                <tr>--}}
+{{--                    <td>{{$parentIncome->name}}</td>--}}
+{{--                    <td>{{$parentIncome->subCategoryiD}}</td>--}}
+{{--                    <td>{{$parentIncome->created_at}}</td>--}}
+{{--                    <td class="text-right">--}}
+{{--                        <a  class="btn btn-warning" href="{{route("CategoryIncome.edit", $parentIncome->id)}}">Edit</a>--}}
+{{--                        <td class="text-xl-center"><form method="post" action="{{route('CategoryIncome.destroy', $parentIncome->id)}}"--}}
+{{--                                  onsubmit='return confirm("Are you sure ?")'>--}}
+{{--                                @csrf--}}
+{{--                                @method('DELETE')--}}
+{{--                                <input type="submit" value="Delete" class="btn btn-danger">--}}
+{{--                            </form></td>--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--            @endforeach--}}
+{{--            </tbody>--}}
+{{--        </table>--}}
+{{--    </div>--}}
+{{--    <script type="text/javascript">--}}
+{{--        function confirmDelete() {--}}
+{{--            var value =  confirm("Sure ?");--}}
+{{--            return value;--}}
+{{--        }--}}
+{{--    </script>--}}
+{{--@endsection--}}
+{{--@section('modalBody')--}}
+{{--    <div class="modal-body">--}}
+{{--        <h5 class="modal-title fw-bold text-center" id="exampleModalLabel">Edit CategoryIncome</h5>--}}
+
+{{--        <form method="post"  action="{{route('CategoryIncome.update',$parentIncome->id)}}">--}}
+{{--            @csrf--}}
+{{--            @method('put')--}}
+{{--            <div>--}}
+{{--                <div>--}}
+{{--                    <label for="name" class="col-md-12 mb-0 mt-2">{{ __('Name') }}</label>--}}
+{{--                    <div class="col-md-12">--}}
+{{--                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"--}}
+{{--                               value="{{ old('name',$parentIncome->name) }}" required autocomplete="name" autofocus>--}}
+
+{{--                        @error('name')--}}
+{{--                        <span class="invalid-feedback" role="alert">--}}
+{{--                                                <strong>{{ $message }}</strong>--}}
+{{--                                            </span>--}}
+{{--                        @enderror--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <div>--}}
+{{--                    <label for="subCategoryiD" class="col-md-12 mb-0 mt-2">{{ __('subCategoryiD') }}</label>--}}
+
+{{--                    <div class="col-md-12">--}}
+{{--                        <input id="subCategoryiD" type="text" class="form-control @error('subCategoryiD') is-invalid @enderror"--}}
+{{--                               name="subCategoryiD" value="{{ old('subCategoryiD',$parentIncome->subCategoryiD) }}" required autocomplete="subCategoryiD">--}}
+
+{{--                        @error('subCategoryiD')--}}
+{{--                        <span class="invalid-feedback" role="alert">--}}
+{{--                                            <strong>{{ $message }}</strong>--}}
+{{--                                            </span>--}}
+{{--                        @enderror--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-md-12">--}}
+{{--                    <div class="row justify-content-around">--}}
+{{--                        <div class="col-4">--}}
+{{--                            <button type="button" class="col btn btn-outline-primary mb-0 mt-2" data-dismiss="modal">--}}
+{{--                                Cacel--}}
+{{--                            </button>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-4">--}}
+{{--                            <button type="submit" class="col btn btn-primary mb-0 mt-2">Save</button>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </form>--}}
+{{--    </div>--}}
+{{--@endsection--}}
